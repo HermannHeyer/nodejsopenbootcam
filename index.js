f3e85c25383de408016f36bd13b2af1ebeee3fb2
Hermann Heyer <h.heyer@docentes.cftmaule.cl>
2022-07-24 18:32:49 -0400

    Ejercicio del Modulo 10

---
+++ index.js
@@ -0,0 +1,6 @@
+import * as matematicas from './modulo/controller.js'
+import chalk from 'chalk';
+const suma = matematicas.suma(3,3)
+console.log(suma)
+const multiplicar = matematicas.multiplicar(3,3)
+console.log(chalk.green(multiplicar))
